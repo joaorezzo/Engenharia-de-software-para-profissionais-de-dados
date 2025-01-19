44193a976696a567be465a89fe3f47f9ccfecd80
joaorezzo <joaorezzoengsoft@gmail.com>
2025-01-18 23:45:40 -0300

    commit 4 - exemplo github

--- arquivo1.py
+++ arquivo1.py
@@ -3,5 +3,6 @@ from funcoes_mat import soma
 nome = 'João Otávio'
 sobrenome = 'Cardoso'       
 
-print(f"Nome: {nome}, ultimo nome {sobrenome}")    
+print(f"Nome: {nome}, ultimo nome {sobrenome}") 
+print(f"Aula de github e acesso de repos")    
 print(soma(2,3))
